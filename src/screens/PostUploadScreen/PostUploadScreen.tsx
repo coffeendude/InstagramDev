401e11b14d4bfa22ccfb9be004044d4bab831c4a
dude <needcoffeedev@gmail.com>
2022-11-17 12:31:50 -0600

    working on photo uploads, ran into naming issues with xcode, saving work

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,12 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {useEffect, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Camera, CameraType} from 'expo-camera';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import colors from '../../theme/colors';
 
 const PostUploadScreen = () => {
-  return (
-    <View>
-      <Text>PostUploadScreen</Text>
-    </View>
-  )
-}
+  //   const [type, setType] = useState(CameraType.back);
+  //   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [hasPermissions, setHasPermissions] = useState<boolean | null>(null);
 
-export default PostUploadScreen
+  useEffect(() => {
+    const getPermission = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
+      setHasPermissions(
+        cameraPermission.status === 'granted' &&
+          microphonePermission.status === 'granted',
+      );
+    };
+    getPermission();
+  }, []);
+
+  if (hasPermissions === null) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (hasPermissions === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.page}>
+      <Camera style={styles.camera} />
+      <View style={styles.buttonsContainer}>
+        <MaterialIcons name="close" size={30} color={colors.white} />
+        <MaterialIcons name="close" size={30} color={colors.white} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: colors.black
+    },
+    camera: {
+        width: "100%",
+        aspectRatio: 3/ 4,
+    },
+    buttonsContainer: {
+
+    },
+})
+
+export default PostUploadScreen;
+
